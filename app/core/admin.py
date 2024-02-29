7a08b33794dde46a2bcf369eb152e7a734f6a8e2
Anish Bade <badeanish6045@gmail.com>
2024-02-29 22:21:24 +0545

    regular commit

--- app/core/admin.py
+++ app/core/admin.py
@@ -4,7 +4,7 @@ Django admin customization.
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.translation import gettext_lazy as _
 from core import models
 
 
@@ -13,5 +13,11 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets =(
+    (None, {'fields': ("email", "password")}),
+    (
+        _
+    )
+    )
 
 admin.site.register(models.User, UserAdmin)
