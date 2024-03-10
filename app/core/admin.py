367d38970bd921905f8f152e7f3852cbf03f0b5d
Anish Bade <badeanish6045@gmail.com>
2024-03-10 20:47:14 +0545

    support modifying users

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,17 +8,26 @@ from django.utils.translation import gettext_lazy as _
 from core import models
 
 
-# class UserAdmin(BaseUserAdmin):
-#     """Define the admin pages for users."""
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
 
-#     ordering = ["id"]
-#     list_display = ["email", "name"]
-#     fieldsets =(
-#     (None, {'fields': ("email", "password")}),
-#     (
-#         _
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
 
-#     )
-#     )
 
-# admin.site.register(models.User, UserAdmin)
+admin.site.register(models.User, UserAdmin)
