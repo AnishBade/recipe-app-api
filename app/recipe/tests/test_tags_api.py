096354c2799bea0a4722bc780e723d9be992ecd2
Anish Bade <badeanish6045@gmail.com>
2024-06-02 22:46:41 +0545

    add public and private api tests

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -13,12 +13,40 @@ from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
-TAGS_URL = reverse('recipe:tag-list')
+TAGS_URL = reverse("recipe:tag-list")
 
-def create_user(email="user@example.com", password = "testpass123"):
+
+def create_user(email="user@example.com", password="testpass123"):
     """Create and return a user."""
     return get_user_model().objects.create_user(email=email, password=password)
-    
 
 
+class PubliTagsApiTests(TestCase):
+    """Test unauthenticated API requests."""
 
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test auth is required for retreiving tags."""
+        res = self.client.get(TAGS_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateTagsApiTests(TestCase):
+    """Test authenticated API requests."""
+
+    def setUp(self):
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_tags(self):
+        """Test retrieving a list of tags."""
+        Tag.objects.create(user=self.user, name="Vegan")
+        Tag.objects.create(user=self.user, name="Dessert")
+
+        res = self.client.get(TAGS_URL)
+
+        tags = Tag.objects.all().order_by("-name")
